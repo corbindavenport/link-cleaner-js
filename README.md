4a16b8b224a493c99292de22d0a04da89d6ff1d8
Corbin Davenport <corbindavenport@outlook.com>
2026-08-04 18:59:38 -0400

    Add initial library and CLI

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # link-cleaner-js
 JavaScript library and CLI for cleaning and modifying URLs.
+
+**This is a work in progress!**
---
+++ src/cli.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+import { cleanLink } from "./main.js";
+
+/**
+ * Get the list of arguments from command line and parse them as an object.
+ * @returns {object}
+ */
+function getArgs() {
+    const array = process.argv;
+    const result = {};
+    for (let i = 0; i < array.length; i++) {
+        const currentItem = array[i];
+        if (currentItem.startsWith('-')) {
+            // Remove all leading dashes
+            const cleanKey = currentItem.replace(/^-+/, '');
+            // Get the value and store it
+            const nextValue = array[i + 1];
+            result[cleanKey] = nextValue;
+        } else {
+            // If there's an argument without a leading dash, assume it's the input URL
+            result["i"] = currentItem;
+        }
+    }
+    return result;
+}
+
+/**
+ * Show help information in the console.
+ */
+function showHelp() {
+    const helpMessage = `Not implemented yet!`;
+    console.log(helpMessage);
+}
+
+/**
+ * Main function
+ */
+function main() {
+    const args = getArgs();
+    if (args.hasOwnProperty("help")) {
+        // Print help if requested
+        showHelp();
+    } else if (args?.i) {
+        const link = cleanLink(args.i).toString();
+        console.log(link);
+    } else {
+        showHelp();
+    }
+}
+
+// Listen for termination signals
+const gracefulShutdown = function () {
+    process.exit(0);
+};
+
+process.on("SIGINT", gracefulShutdown);
+process.on("SIGTERM", gracefulShutdown);
+
+// Start main process
+main();
+gracefulShutdown();
